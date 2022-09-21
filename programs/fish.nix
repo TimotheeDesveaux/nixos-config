bbc32fcb4127da2e3259528abbefa7e4be896e17
Timothée Desveaux <timothee.desveaux@epita.fr>
2022-09-21 09:56:27 +0200

    fish: update aliases and abreviations

--- programs/fish.nix
+++ programs/fish.nix
@@ -14,23 +14,35 @@
       vim = "nvim";
 
       ccf =
-        "gcc -Wextra -Wall -Werror -std=c99 -pedantic -fsanitize=address -o main";
+        "gcc -Wextra -Wall -Werror -std=c99 -pedantic -g -O0 -fno-inline -fsanitize=address -o main";
       cppf = "g++ -Wextra -Wall -Werror -std=c++17 -pedantic -o main";
       valgrind-full =
         "valgrind --leak-check=full --show-leak-kinds=all --leak-resolution=high --track-origins=yes --vgdb=yes";
 
-      gd = "git diff";
       ga = "git add";
       gc = "git commit";
+      gd = "git diff";
+      gf = "git fetch";
+      gl = "git log";
       gp = "git push";
+      gP = "git pull";
+      gr = "git reset";
+      gR = "git restore";
+      gs = "git status";
+      gS = "git switch";
 
       nd = "nix develop --command fish";
       ns = "nix-shell --command fish";
+
+      dc = "docker container";
+      dC = "docker compose";
+
+      bt = "bluetoothctl";
     };
 
     shellAliases = {
-      gl = "git log --all --decorate --oneline --graph --color=always";
-      gs = "git status -s";
+      gL = "git log --all --decorate --oneline --graph --color=always";
+      gg = "git status -s";
 
       zathura = "zathura --fork";
 
