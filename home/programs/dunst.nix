fbc24757c6214be64b9fd1cc149e15a66698b742
Timothée Desveaux <timothee.desveaux@gmail.com>
2024-04-19 17:25:42 +0200

    programs(dunst): configure it
    62 2024-04-19 17:25:37 24.05.20240416.5672bc9 6.6.27
    63 2024-04-19 17:43:44 24.05.20240416.5672bc9 6.6.27
    64 2024-04-19 17:45:42 24.05.20240416.5672bc9 6.6.27
    65 2024-04-19 17:46:54 24.05.20240416.5672bc9 6.6.27
    66 2024-04-19 17:48:18 24.05.20240416.5672bc9 6.6.27
    67 2024-04-19 17:49:24 24.05.20240416.5672bc9 6.6.27
    68 2024-04-19 17:51:31 24.05.20240416.5672bc9 6.6.27
    69 2024-04-19 17:52:28 24.05.20240416.5672bc9 6.6.27

--- home/programs/dunst.nix
+++ home/programs/dunst.nix
@@ -3,5 +3,54 @@
 {
   services.dunst = {
     enable = true;
+    settings = with config.theme; {
+      global = {
+        follow = "mouse";
+        width = 350;
+        offset = "5x15";
+        notification_limit = 6;
+        separator_height = 2;
+        horizontal_padding = 10;
+        frame_width = 0;
+        separator_color = "#${colors.black}";
+        idle_threshold = 120;
+
+        # Text
+        font = "Hack Nerd Font 10";
+        format = "<b>%s</b> %p\n%b";
+
+        # Icons
+        min_icon_size = 32;
+        max_icon_size = 64;
+      #   icon_path =
+      #     "/home/stolen/.icons/tokyonight/status/22:/home/stolen/.icons/tokyonight/devices/22:/home/stolen/.icons/tokyonight/panel/22";
+
+        # Misc/Advanced
+        browser = "${pkgs.firefox}/bin/firefox -new-tab";
+        corner_radius = 5;
+
+        # Mouse
+        mouse_middle_click = "close_all";
+        mouse_right_click = "do_action, close_current";
+      };
+
+      urgency_low = {
+        background = "#${colors.bg_dark}";
+        foreground = "#${colors.fg}";
+        timeout = 10;
+      };
+
+      urgency_normal = {
+        background = "#${colors.bg_dark}";
+        foreground = "#${colors.blue}";
+        timeout = 10;
+      };
+
+      urgency_critical = {
+        background = "#${colors.bg_dark}";
+        foreground = "#${colors.red}";
+        timeout = 0;
+      };
+    };
   };
 }
