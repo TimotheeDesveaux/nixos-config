824611bac802f00c1f2bb8fc603358f79b963b4b
Timothée Desveaux <timothee.desveaux@gmail.com>
2025-09-15 19:28:47 +0200

    configuration: enable avahi
    374 2025-09-15 19:28:44 25.11.20250819.2007595 6.12.42
    375 2025-09-08 19:06:31 25.11.20250819.2007595 6.12.42
    376 2025-09-15 20:20:47 25.11.20250819.2007595 6.12.42
    377 2025-09-15 20:26:56 25.11.20250819.2007595 6.12.42

--- hosts/nixos-lenovo-stolen/configuration.nix
+++ hosts/nixos-lenovo-stolen/configuration.nix
@@ -92,6 +92,11 @@
   # Enable the OpenSSH daemon
   services.openssh.enable = true;
 
+  services.avahi = {
+    enable = true;
+    nssmdns = true;
+  };
+
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #
